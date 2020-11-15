3c65a8bf722b7a14ff169ba78a7c9748353776c9
Chak Hon Lam <chakhon@hawaii.edu>
2020-11-14 22:01:08 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Header, Button, Dropdown } from 'semantic-ui-react';
+import { Header, Button } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
-    const introStyle = { marginTop: '15px',  marginBottom: '15px'};
+    const introStyle = { marginTop: '15px', marginBottom: '15px' };
     return (
         <div>
           <div className="digits-landing-background"/>
           <div style={introStyle} className="ui center aligned container">
             <Header size='huge'>WELCOME TO COPING WITH COVID</Header>
             <p className="intro">
-              Student mental health in higher education has been an increasing concern. The COVID-19 pandemic has negatively affected everybody, causing a lot of pressure, uneasiness, despondency, and inner issues for the understudies alongside their companions and their family. We propose to create a mental health page that can give students ease and comfort. Making therapy and relief accessible to all. It will be a safe space where people can openly express themselves without fear of judgment.
+              Student mental health in higher education has been an increasing concern.
+               The COVID-19 pandemic has negatively affected everybody, causing a lot of pressure, uneasiness, despondency,
+              and inner issues for the understudies alongside their companions and their family.
+               We propose to create a mental health page that can give students ease and comfort.
+               Making therapy and relief accessible to all. It will be a safe space where people can openly express themselves without fear of judgment.
             </p>
 
             <Button circular size='large' as={NavLink} exact to="/signup">Log in</Button>
