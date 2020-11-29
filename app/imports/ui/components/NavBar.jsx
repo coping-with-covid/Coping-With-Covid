55c307df41c1a7ef3efd737fbb829a67afe4f06f
guanhongl <guanh.sl23@gmail.com>
2020-11-28 18:58:34 -1000

    #3&4 updated profile navlink

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,21 +3,28 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Dropdown, Header } from 'semantic-ui-react';
+import { Menu, Dropdown, Header, Loader } from 'semantic-ui-react';
 import { Roles } from 'meteor/alanning:roles';
+import { Profiles } from '../../api/profile/Profiles';
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 class NavBar extends React.Component {
+
+  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  renderPage() {
     const menuStyle = { height: '50px', border: '0px', margin: '0px' };
     return (
         <div>
           <Menu style={menuStyle} widths={5} key='menuBar' attached="top" borderless>
             <Menu.Item/>
             <Menu.Item/>
-              <Menu.Item as={NavLink} activeClassName="" exact to="/">
-                <Header className="ui huge header title">COPING WITH COVID</Header>
-              </Menu.Item>
+            <Menu.Item as={NavLink} activeClassName="" exact to="/">
+              <Header className="ui huge header title">COPING WITH COVID</Header>
+            </Menu.Item>
             <Menu.Item/>
             <Menu.Item position="right">
               {this.props.currentUser === '' ? (
@@ -38,13 +45,13 @@ class NavBar extends React.Component {
           </Menu>
           {this.props.currentUser ? (
               <Menu style={menuStyle} widths={12} size='huge' attached='top' borderless>
-                  <Menu.Item as={NavLink} exact to="/">Home</Menu.Item>
-                  <Menu.Item as={NavLink} exact to="/list">Forum</Menu.Item>
-                  <Menu.Item as={NavLink} exact to="/add">FAQs</Menu.Item>
-                  <Menu.Item as={NavLink} exact to="/profile">Profile</Menu.Item>
-                  {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-                      <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
-                  ) : ''}
+                <Menu.Item as={NavLink} exact to="/">Home</Menu.Item>
+                <Menu.Item as={NavLink} exact to="/list">Forum</Menu.Item>
+                <Menu.Item as={NavLink} exact to="/add">FAQs</Menu.Item>
+                <Menu.Item as={NavLink} exact to={`/profile/${this.props.profile._id}`}>Profile</Menu.Item>
+                {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+                    <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
+                ) : ''}
               </Menu>
           ) : ''}
         </div>
@@ -55,12 +62,22 @@ class NavBar extends React.Component {
 /** Declare the types of all properties. */
 NavBar.propTypes = {
   currentUser: PropTypes.string,
+  profile: PropTypes.object,
+  ready: PropTypes.bool,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-const NavBarContainer = withTracker(() => ({
-  currentUser: Meteor.user() ? Meteor.user().username : '',
-}))(NavBar);
+const NavBarContainer = withTracker(() => {
+  const subscription = Meteor.subscribe(Profiles.userPublicationName);
+  const currentUser = Meteor.user() ? Meteor.user().username : '';
+  const ready = subscription.ready();
+  const profile = Profiles.collection.findOne({ owner: currentUser });
+  return {
+    currentUser,
+    ready,
+    profile,
+  };
+})(NavBar);
 
 /** Enable ReactRouter for this component. https://reacttraining.com/react-router/web/api/withRouter */
 export default withRouter(NavBarContainer);
