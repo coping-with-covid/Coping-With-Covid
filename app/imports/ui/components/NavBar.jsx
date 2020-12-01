4cd841bee460ee7e4b98049606fe313e9b135292
Chak Hon Lam <chakhon@hawaii.edu>
2020-11-30 23:07:40 -1000

    Fix test error due to navbar link

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -46,8 +46,8 @@ class NavBar extends React.Component {
           {this.props.currentUser ? (
               <Menu style={menuStyle} widths={12} size='huge' attached='top' borderless>
                 <Menu.Item as={NavLink} exact to="/home">Home</Menu.Item>
-                <Menu.Item id="websites" as={NavLink} exact to="/forums">Forum</Menu.Item>
-                <Menu.Item as={NavLink} exact to="/websites">Websites</Menu.Item>
+                <Menu.Item as={NavLink} exact to="/forums">Forum</Menu.Item>
+                <Menu.Item id="websites" as={NavLink} exact to="/websites">Websites</Menu.Item>
                 <Menu.Item as={NavLink} exact to="/about">FAQs</Menu.Item>
                 <Menu.Item id="profile" as={NavLink} exact to={`/profile/${this.props.profile._id}`}>Profile</Menu.Item>
                 {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
